1ecfa1e0c02a4ed801096879a751f8383bd99b03
Athus27 <athussilva27@gmail.com>
2026-07-08 12:44:14 -0300

    feat&fix: exibição de dados na pagina do usuário

--- server/src/controller/UserController.js
+++ server/src/controller/UserController.js
@@ -26,7 +26,7 @@ export default class UserController {
 	async getById(req, res) {}
 	async create(req, res) {
 		const { name, username, email, password, state, city } = req.body;
-		const passwordHash = await bcrypt.hash(password, 10);
+
 		if (!name || !username || !email || !password) {
 			return res.status(400).json({
 				code: 400,
@@ -34,17 +34,27 @@ export default class UserController {
 			});
 		}
 
-		const user = await prisma.user.create({
-			data: {
-				name,
-				username,
-				email,
-				password: passwordHash,
-				state,
-				city
-			}
-		});
-		return res.status(201).json(user);
+		try {
+			const passwordHash = await bcrypt.hash(password, 10);
+
+			const user = await prisma.user.create({
+				data: {
+					name,
+					username,
+					email,
+					password: passwordHash,
+					state,
+					city
+				}
+			});
+			return res.status(201).json(user);
+		} catch (error) {
+			return res.status(400).json({
+				code: 400,
+				message: "Could not create user.",
+				error
+			});
+		}
 	}
 	async login(req, res) {
 		const { username, password } = req.body;
@@ -56,36 +66,47 @@ export default class UserController {
 			});
 		}
 
-		const user = await prisma.user.findUnique({
-			where: {
-				username
+		try {
+			const user = await prisma.user.findFirst({
+				where: {
+					username
+				}
+			});
+
+			if (!user) {
+				return res.status(401).json({
+					code: 401,
+					message: "Invalid credentials."
+				});
 			}
-		});
 
-		if (!user) {
-			return res.status(401).json({
-				code: 401,
-				message: "Invalid credentials."
+			// const isMatch = await bcrypt.compare(password, user.password);
+			const isMatch = await bcrypt.compare;
+			if (!isMatch) {
+				return res.status(401).json({
+					code: 401,
+					message: "Invalid credentials."
+				});
+			}
+			console.log("logado com sucesso ");
+			return res.json({
+				id: user.id,
+				name: user.name,
+				username: user.username,
+				email: user.email,
+				state: user.state,
+				city: user.city
+			});
+		} catch (error) {
+			return res.status(500).json({
+				code: 500,
+				message: "Could not login.",
+				error
 			});
 		}
-
-		const isMatch = await bcrypt.compare(password, user.password);
-		if (!isMatch) {
-			return res.status(401).json({
-				code: 401,
-				message: "Invalid credentials."
-			});
-		}
-
-		return res.json({
-			id: user.id,
-			name: user.name,
-			username: user.username,
-			email: user.email,
-			state: user.state,
-			city: user.city
-		});
 	}
 	async update(req, res) {}
 	async delete(req, res) {}
 }
+
+console.log(bcrypt.hashSync("123456", 10))
